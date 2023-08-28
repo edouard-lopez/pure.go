fad59d074c199c04bf769e661172c462d0590c9e
Édouard Lopez <contact@edouard-lopez.com>
2023-08-28 22:59:47 +0200

    fix: return `go version` instead of version used to build binary

--- internal/segments/go_version/go_version_test.go
+++ internal/segments/go_version/go_version_test.go
@@ -1,30 +1,55 @@
 package go_version
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
+var FakeGoVersionOutput = "go version go1.21.0 linux/amd64"
+
+func Test_Parse_Go_version(t *testing.T) {
+	output := FakeGoVersionOutput
+	expectedVersion := "1.21.0"
+
+	actual := Parse(output)
+
+	assert.Equal(t, expectedVersion, actual)
+}
+
 type MockedRuntime struct {
 	mock.Mock
 }
 
-func (m *MockedRuntime) Version() string {
+func (m *MockedRuntime) Output() ([]byte, error) {
 	args := m.Called()
-	return args.String(0)
+	return []byte(args.String(0)), errors.New("Missing go binary")
 }
 
-func Test_Get(t *testing.T) {
+func Test_Get_a_version(t *testing.T) {
 	mockRuntime := new(MockedRuntime)
-	expectedVersion := "0.0.1"
-	mockRuntime.On("Version").Return(expectedVersion)
-	GetGoVersion = mockRuntime.Version // redefine getGoVersion to use the mock
+	expectedVersion := "1.21.0"
+	mockRuntime.On("Output").Return(FakeGoVersionOutput)
+	GetGoVersion = mockRuntime.Output // redefine go_version.GetGoVersion to use the mock
 
-	result := Get()
+	actual := Get()
 
-	assert.Equal(t, expectedVersion, result)
+	assert.Equal(t, expectedVersion, actual)
+
+	mockRuntime.AssertExpectations(t)
+}
+
+func Test_Get_no_version(t *testing.T) {
+	mockRuntime := new(MockedRuntime)
+	expectedVersion := ""
+	mockRuntime.On("Output").Return(expectedVersion)
+	GetGoVersion = mockRuntime.Output // redefine go_version.GetGoVersion to use the mock
+
+	actual := Get()
+
+	assert.Equal(t, expectedVersion, actual)
 
 	mockRuntime.AssertExpectations(t)
 }
